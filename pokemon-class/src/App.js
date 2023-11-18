14bc9f64fd6ebf836b79524471f30ed5d652eeea
Lewbagz <lewishardie@gmail.com>
2023-11-18 13:17:06 +1100

    edit to auto deploy

--- pokemon-class/src/App.js
+++ pokemon-class/src/App.js
@@ -5,7 +5,7 @@ import PokemonDisplay from './PokemonDisplay';
 function App() {
   return (
     <div className="App">
-      <h1>Class components lesson</h1>
+      <h1>Class components lesson. Pokemon Name</h1>
       <PokemonDisplay />
     </div>
   );
