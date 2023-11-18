bc7e81915ec6ae12fae82c960e0511130d776a65
Lewbagz <lewishardie@gmail.com>
2023-11-18 12:42:24 +1100

    mounting and unmounting

--- pokemon-class/src/PokemonDisplay.jsx
+++ pokemon-class/src/PokemonDisplay.jsx
@@ -4,17 +4,27 @@ import React from "react"
 export default class PokemonDisplay extends React.Component{
     constructor(props){
         super(props);
+        this.name = "Pikachu"
         
         //-- State stores data per component
         this.state = {
             pokemonName: null
         }
+    }
 
+    componentDidMount(){
+        this.name = "Bulbasaur";
+        console.log(this.name);
+        console.log("Did mount triggers when...")
+    }
+
+    componentWillUnmount(){
+        console.log("Will unmount triggers when...")
     }
 
     render() {
         return(
-            <h2>Pokemon name</h2>
+            <h2>Pokemon {this.name}</h2>
         )
     }
 }
